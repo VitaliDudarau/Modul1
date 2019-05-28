fec5c34f5853dbeb7f28a3b0871b1888f4505473
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-05-28 23:26:54 +0300

    Add Task 1 Modul 1

---
+++ src/com/exemple/task1/Task1.java
@@ -0,0 +1,16 @@
+package com.exemple.task1;
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        double a, b, c, z;
+        Scanner s = new Scanner(System.in);
+        System.out.println("write a");
+        a = s.nextDouble();
+        System.out.println("write b");
+        b = s.nextDouble();
+        System.out.println("write c");
+        c = s.nextDouble();
+        z = ((a - 3) * b / 2) + c;
+        System.out.println("final value is " + z);
+    }
+}
