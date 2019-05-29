b64ae922c4246ba585572150ad9528125185a83e
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-05-29 22:02:29 +0300

    Modul 1 Task 2

---
+++ src/com/exemple/task1/Task2.java
@@ -0,0 +1,24 @@
+package com.exemple.task1;
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        int a, b, c;
+        double x, y, z;
+        Scanner s = new Scanner(System.in);
+        System.out.println("write a");
+        a = s.nextInt();
+        if (a == 0) {
+            System.out.println("division by 0");
+        }else {
+            System.out.println("write b");
+            b = s.nextInt();
+            System.out.println("write c");
+            c = s.nextInt();
+            x = (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c) / (2 * a));
+            y = Math.pow(a, 3) * c - Math.pow(b, -2);
+            z = x - y;
+            System.out.println("final value is " + z);
+        }
+    }
+}
+
