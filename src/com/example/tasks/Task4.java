1b6dee2addc13747ce3e5c39df84f8273d46d58c
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-05-30 22:56:38 +0300

    Add Task 4 Modul 1

---
+++ src/com/example/tasks/Task4.java
@@ -0,0 +1,19 @@
+package com.example.tasks;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        double R, y, xd, z;
+        int x;
+        Scanner s = new Scanner(System.in);
+        System.out.println("write double R (format nnn.ddd)");
+        R = s.nextDouble();
+        x = (int)R;
+        xd =  (double)x / 1000;
+        y = (R - x) *1000;
+        z = Math.round(y) + xd;
+        System.out.print("reverse value is ");
+        System.out.printf("%8.3f", z);
+    }
+}
