5a24f7540bb5a79747f598dc4d033aa6b0dd4ac3
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-06-16 20:46:30 +0300

    Add Task 6 Modul 1

---
+++ src/com/example/tasks/Task6.java
@@ -0,0 +1,32 @@
+package com.example.tasks;
+
+import java.util.Scanner;
+
+public class Task6 {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+
+        int x, y;
+        boolean a;
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("write y");
+        y = s.nextInt();
+
+        if(y < -3 || y > 4) {
+            a = false;
+            System.out.println(a);
+        }else {
+            System.out.println("write x");
+            x = s.nextInt();
+            if ((y <= 0 && x >= -4 && x <= 4) || (y > 0 && x >= -2 && x <= 2)) {
+                a = true;
+                System.out.println(a);
+            }else {
+                a = false;
+                System.out.println(a);
+            }
+        }
+    }
+}
