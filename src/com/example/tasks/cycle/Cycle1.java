60923f30dc8dd6b24bac080f5b116f57fd7e7436
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-06-23 22:40:33 +0300

    Add Cycle1 Modul1

---
+++ src/com/example/tasks/cycle/Cycle1.java
@@ -0,0 +1,29 @@
+/*
+1. Напишите программу, где пользователь вводит любое целое положительное число.
+А программа суммирует все числа от 1 до введенного пользователем числа.
+ */
+package com.example.tasks.cycle;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Cycle1 {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+
+        int a, n = 0;
+        BigInteger sum = new BigInteger("0");
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("write a : ");
+        a = s.nextInt();
+
+        while (n < a) {
+            n += 1 ;
+            sum = sum.add(BigInteger.valueOf(n));
+        }
+
+        System.out.println("sum of numbers is " + sum);
+    }
+}
