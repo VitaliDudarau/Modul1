7649b0c9ae6bd33e8908217f0dac394d78ad7dbd
VitaliDudarau <dudarev.vitalii@gmail.com>
2019-07-01 22:00:21 +0300

    Change Cycle8 Modul1 - 2525 && 24225  answer is 2 and 5

--- src/com/example/tasks/cycle/Cycle8.java
+++ src/com/example/tasks/cycle/Cycle8.java
@@ -11,7 +11,9 @@ public class Cycle8 {
     @SuppressWarnings("resources")
     public static void main(String[] args) {
 
-        int a, b, c, x, y, count = 0;
+        int a, b, c, d, x, y, count = 0, count0 = 0, count1 = 0,
+        count2 = 0, count3 = 0, count4 = 0, count5 = 0,
+        count6 = 0, count7 = 0, count8 = 0, count9 = 0;
 
         Scanner s = new Scanner(System.in);
         System.out.println("write a: ");
@@ -27,8 +29,69 @@ public class Cycle8 {
             while (c > 0) {
                 y = c % 10;
                 if (x == y) {
-                    System.out.print(y + " | ");
                     count++;
+                    switch (y) {
+                        case 0: count0++;
+                                if (count0 > 1) {
+                                   break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 1: count1++;
+                                if (count1 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 2: count2++;
+                                if (count2 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 3: count3++;
+                                if (count3 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 4: count4++;
+                                if (count4 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 5: count5++;
+                                if (count5 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 6: count6++;
+                                if (count6 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 7: count7++;
+                                if (count7 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 8: count8++;
+                                if (count8 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                        case 9: count9++;
+                                if (count9 > 1) {
+                                    break;
+                                }
+                                System.out.print(y + " | ");
+                                break;
+                    }
                     break;
                 }
                 c = c / 10;
